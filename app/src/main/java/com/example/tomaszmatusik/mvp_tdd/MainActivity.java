d39c089e7619e133ff24d0cf9756e238806dd6cd
tomaszmatusik <tomasz.matusik95@gmail.com>
2018-11-01 19:02:40 +0100

    implemented data binding

--- app/src/main/java/com/example/tomaszmatusik/mvp_tdd/MainActivity.java
+++ app/src/main/java/com/example/tomaszmatusik/mvp_tdd/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.tomaszmatusik.mvp_tdd;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.tomaszmatusik.mvp_tdd.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
